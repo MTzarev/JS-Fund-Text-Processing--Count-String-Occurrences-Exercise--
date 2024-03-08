c08a40de8d898618e50a3fc9d349d1db113a3279
Miroslav Tzarev <mtzarev@gmail.com>
2024-03-08 23:25:35 +0200

    Add files via upload

---
+++ 4.Count String Occurrences.js
@@ -0,0 +1,15 @@
+function countStringOccurrences(text, searchedWord){
+let words = text.split(` `)
+let counter = 0
+for (const word of words) {
+    if(word === searchedWord){
+        counter++
+    }
+    
+}
+    console.log(counter);
+}
+countStringOccurrences('This is a word and it also is a sentence',
+'is');
+countStringOccurrences('softuni is great place for learning new programming languages',
+'softuni');
